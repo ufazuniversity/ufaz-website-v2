a711da13567530b5bd46afe8cc20e351ee90b84a
Gadir Rustamli <g.rustamli@ufaz.az>
2022-12-28 19:31:54 +0400

    Added a command to docker-entrypoint.sh to createsuperuser automatically

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-set -xe; python manage.py migrate --noinput; gunicorn website.wsgi:application
+set -xe; python manage.py migrate --noinput
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+  python manage.py createsuperuser \
+  --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL
+fi
+
+gunicorn website.wsgi:application
