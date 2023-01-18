7e0102a8a938771a1374d9b008f100f2e523db26
Gadir Rustamli <g.rustamli@ufaz.az>
2023-01-18 12:13:48 +0400

    Installed django-watchfiles to trigger autoreload on custom file changes

--- website/settings/base.py
+++ website/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "wagtailmenus",
     "modelcluster",
     "taggit",
+    "django_watchfiles",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
