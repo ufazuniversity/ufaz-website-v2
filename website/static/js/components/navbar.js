db20870581ea0dddb2ae0f9391259c66e6b5d6bc
Gadir Rustamli <g.rustamli@ufaz.az>
2023-01-24 15:28:30 +0400

    coded the toggling of mobile navigation

--- website/static/js/components/navbar.js
+++ website/static/js/components/navbar.js
@@ -1,6 +1,18 @@
-const siteHeader = document.querySelector("#navigation");
-const headerToggle = document.querySelector(".mobile-nav-toggle");
+const navigation = document.querySelector("#navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-export default function initiateHeader() {
-  console.log("Hello");
-}
+const DATA_VISIBLE = "data-visible";
+
+// Toggle navbar on mobile view
+navToggle.addEventListener("click", () => {
+  const visible = navigation.getAttribute("data-visible");
+  if (visible == "false") {
+    navigation.setAttribute(DATA_VISIBLE, true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visible == "true") {
+    navigation.setAttribute(DATA_VISIBLE, false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+export default {};
